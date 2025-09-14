f33ef13262cc4f14612494ce30eed3b2a63f2031
saikumar15796 <oddemsai157@gmail.com>
2025-09-14 22:48:07 +0530

    new

--- src/home.js
+++ src/home.js
@@ -11,23 +11,24 @@ function Home (prop){
  <div className="custom-bg">
     <div className="row ">
         <h2 className="mt-2" style={{marginLeft:"50px"}}>Protfolio</h2>
-       <div className="col-lg-6 main-row  text-white ">
-         <Outlet></Outlet>
-       </div>
-       <div className="col-lg-6   text-white ">
-            <div className="navbar-links">
+         <div className="navbar-links">
             <Link to="/">HOME</Link>
             <Link to="/about">ABOUT</Link>
             <Link to="/contact">CONTACT</Link>
             <Link to="/projects">PROJECTS</Link>
             </div>
-            <div className="text-center mt-5">
+       <div className="col-lg-6 main-row  text-white ">
+         <Outlet></Outlet>
+       </div>
+       <div className="col-lg-6   text-white mt-5 ">
+           
+            <div className="text-center mt-5" >
                 <img
                     src={`${process.env.PUBLIC_URL}/01.jpg`}  // replace with your image URL or import
                     alt="Profile"
                     className="rounded-circle border border-3 border-info"
-                    width="450"
-                    height="450"
+                    width="400"
+                    height="400"
                 />
            </div>
         </div>
