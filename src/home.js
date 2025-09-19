2ce3ab9818d2892ba89a66c76b1a263f68a6099d
saikumar15796 <oddemsai157@gmail.com>
2025-09-19 22:27:47 +0530

    hole page

--- src/home.js
+++ src/home.js
@@ -1,55 +1,46 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
-function Home() {
+function Home(prop) {
   return (
-    <div className="custom-bg min-vh-100">
-      {/* Navbar */}
-      <nav className="navbar navbar-expand-lg px-4">
-        <div className="container-fluid">
-          <h2 className="text-white">Portfolio</h2>
-
-          {/* Mobile Toggle Button */}
-          <button
-            className="navbar-toggler bg-light"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          {/* Navbar Links */}
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <div className="ms-auto">
-              <Link className="nav-link d-inline-block mx-2 text-white" to="/">HOME</Link>
-              <Link className="nav-link d-inline-block mx-2 text-white" to="/about">ABOUT</Link>
-              <Link className="nav-link d-inline-block mx-2 text-white" to="/contact">CONTACT</Link>
-              <Link className="nav-link d-inline-block mx-2 text-white" to="/projects">PROJECTS</Link>
+    <>
+      <div className="custom-bg container-fluid">
+        {/* Navbar Section */}
+        <div className="row align-items-center py-3">
+          <div className="col-12 col-md-6">
+            <h2 className="text-white ms-md-5 text-center text-md-start">
+              Portfolio
+            </h2>
+          </div>
+          <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-end mt-2 mt-md-0">
+            <div className="navbar-links d-flex gap-3">
+              <Link to="/">HOME</Link>
+              <Link to="/about">ABOUT</Link>
+              <Link to="/contact">CONTACT</Link>
+              <Link to="/projects">PROJECTS</Link>
             </div>
           </div>
         </div>
-      </nav>
 
-      {/* Content */}
-      <div className="row d-flex align-items-center text-white p-4">
-        {/* Left Section */}
-        <div className="col-12 col-lg-6 mb-4 mb-lg-0 text-center text-lg-start">
-          <Outlet />
-        </div>
+        {/* Main Content */}
+        <div className="row align-items-center text-white py-5">
+          {/* Left Section (Text/Outlet) */}
+          <div className="col-12 col-md-6 mb-4 mb-md-0 text-center text-md-start px-4">
+            <Outlet />
+          </div>
 
-        {/* Right Section (Profile Image) */}
-        <div className="col-12 col-lg-6 text-center">
-          <img
-            src={`${process.env.PUBLIC_URL}/01.jpg`}
-            alt="Profile"
-            className="rounded-circle border border-3 border-info img-fluid mt-4"
-            width="280"
-            height="280"
-          />
+          {/* Right Section (Profile Image) */}
+          <div className="col-12 col-md-6 text-center">
+            <img
+              src={`${process.env.PUBLIC_URL}/01.jpg`}
+              alt="Profile"
+              className="rounded-circle border border-3 border-info img-fluid"
+              style={{ maxWidth: "220px", width: "100%", height: "auto" }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
